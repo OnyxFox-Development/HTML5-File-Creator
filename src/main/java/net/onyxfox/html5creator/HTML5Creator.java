235e8d2ca7350017c81b74e33bf3df862300ac2b
linuxdemon <walter@walterbarnes.net>
2014-09-15 01:01:39 -0500

    Base Project Setup

---
+++ src/main/java/net/onyxfox/html5creator/HTML5Creator.java
@@ -0,0 +1,12 @@
+package net.onyxfox.html5creator;
+
+public class HTML5Creator
+{
+	public static void main(String[] args)
+	{
+		for (String arg : args)
+		{
+			System.out.println(arg);
+		}
+	}
+}

1f2531c3ad2f4731336e94c223cf8518ecb1bcaf
linuxdemon <walter@walterbarnes.net>
2014-09-15 03:09:29 -0500

    Stuff

--- src/main/java/net/onyxfox/html5creator/HTML5Creator.java
+++ src/main/java/net/onyxfox/html5creator/HTML5Creator.java
@@ -1,12 +1,15 @@
 package net.onyxfox.html5creator;
 
+import java.util.Scanner;
+
 public class HTML5Creator
 {
 	public static void main(String[] args)
 	{
-		for (String arg : args)
-		{
-			System.out.println(arg);
-		}
+		Scanner uInput = new Scanner(System.in);
+		
+		System.out.print("Filename: ");
+		String filename = uInput.next();
+		System.out.println(String.format("You set %s as the filename", filename));
 	}
 }
